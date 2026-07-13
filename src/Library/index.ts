ac972294d2837e1c129474524be32d4bcb01f55f
Giorgio Pagani <giorgiopagani.tech@gmail.com>
2026-07-13 09:47:56 +0200

    add export components

--- src/Library/index.ts
+++ src/Library/index.ts
@@ -1,5 +1,14 @@
-import { VxButton } from '@/Library/hooks/Button/VxButton';
-import { VxInput } from '@/Library/hooks/Button/VxInput';
+export { default as VxButton } from '@/Library/components/Button/VxButton.vue';
+export { default as VxInput } from '@/Library/components/Input/VxInput.vue';
+export { default as VxCheckbox } from '@/Library/components/Input/VxCheckbox.vue';
+export { default as VxColorPicker } from '@/Library/components/Input/VxColorPicker.vue';
+export { default as VxDatePicker } from '@/Library/components/Input/VxDatePicker.vue';
+export { default as VxDateRange } from '@/Library/components/Input/VxDateRange.vue';
+export { default as VxDateTimePicker } from '@/Library/components/Input/VxDateTimePicker.vue';
+export { default as VxDateTimeRange } from '@/Library/components/Input/VxDateTimeRange.vue';
+export { default as VxRadio } from '@/Library/components/Input/VxRadio.vue';
+export { default as VxRange } from '@/Library/components/Input/VxRange.vue';
+export { default as VxTimePicker } from '@/Library/components/Input/VxTimePicker.vue';
 export { createApiClient, VxApiConfig } from '@/Library/core/composables/Api/apiConfig';
 export { useVxApi } from '@/Library/hooks/Api/useVxApi';
 export type {
